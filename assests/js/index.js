4cd8a5ab8cd1021b129182c610fccea87b98f845
Margaux <margaux.santos113@gmail.com>
2024-02-11 14:33:05 -0800

    readme, folders, and apis

---
+++ assests/js/index.js
@@ -0,0 +1,9 @@
+// html elements
+
+// APIs
+const weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e03184690f717860154f954a4e25bb32";
+const geoAPI = "api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid=e03184690f717860154f954a4e25bb32";
+
+// functions
+
+// event listeners
